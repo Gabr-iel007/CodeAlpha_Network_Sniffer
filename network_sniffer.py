da6b7713cbdffdfb4ec832cdcabf049d0e7b1949
Gabr-iel <kayodeolawunmi117@gmail.com>
2026-05-28 15:57:10 +0100

    added comments

--- network_sniffer.py
+++ network_sniffer.py
@@ -2,12 +2,12 @@ from scapy.all import *
 
 def packet_info(packet):
     
-    if packet.haslayer(IP):
+    if packet.haslayer(IP): #check if the packet contains an IP layer
 
-        print("Source IP:", packet[IP].src)
-        print("Destination IP:", packet[IP].dst)
+        print("Source IP:", packet[IP].src) #get IP source
+        print("Destination IP:", packet[IP].dst) #get IP destination
 
-        protocol = packet[IP].proto
+        protocol = packet[IP].proto # get protocol number
 
         if protocol == 6:
              print("Protocol: TCP")
@@ -18,10 +18,10 @@ def packet_info(packet):
         elif protocol == 1:
             print("Protocol: ICMP")
 
-        if packet.haslayer(Raw):
+        if packet.haslayer(Raw): #check if packet contains actual data
 
             print ("Payload:")
-            print(packet[Raw].load)
+            print(packet[Raw].load)# print raw extracted payload bytes
         print("-" * 40)
 
-sniff(prn=packet_info, count=5)
+sniff(prn=packet_info, count=5) #number of tries before disengaging
